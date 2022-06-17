bcea513728d818bfebd46e5519ea79134de3b19d
yididiya1 <yididiyakebede@gmail.com>
2022-06-18 00:19:59 +0300

    Login page implemented

---
+++ app/templates/login.php
@@ -0,0 +1,50 @@
+
+<html>
+<head>
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+
+<body>
+
+<div class="wrapper login">
+        <div class="container">
+            <div class="col-left">
+                <div class="login-text">
+                    <h2>Welcome to,</h2>
+                    <p>Addis Ababa City Feedback<br>System</p>
+                    
+                </div>
+            </div>
+
+            <div class="col-right">
+                <div class="signup">
+                    <a href="signup.php" class="btn">Sign Up</a>
+                </div>
+                <div class="login-form">
+                    <h2>Login</h2>
+                    <form action="">
+                        <p>
+                            <label>Username/Email address<span>*</span></label>
+                            <input type="text" placeholder="Username or Email" required>
+                        </p>
+                        <p>
+                            <label>Password<span>*</span></label>
+                            <input type="password" placeholder="Password" required>
+                        </p>
+                        <p>
+                            <input type="submit" value="Log In">
+                        </p>
+                        <p>
+                            <a href="">Forgot password?</a>
+                        </p>
+
+                    </form>
+                </div>
+                
+                
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ app/templates/signup.php
@@ -0,0 +1,73 @@
+
+<html>
+<head>
+    <link rel="stylesheet" href="./css/signup.css">
+</head>
+
+<body>
+
+<div class="wrapper login">
+        <div class="container">
+            <div class="col-left">
+                <div class="login-text">
+                    <h2>Welcome to,</h2>
+                    <p>Addis Ababa City Feedback<br>System</p>
+                    <p>Sign Up page</p>
+                </div>
+            </div>
+
+            <div class="col-right">
+                <div class="signup">
+                    <a href="login.php" class="btn">Sign In</a>
+                </div>
+                <div class="login-form">
+                    <h2>Login</h2>
+                    <form action="">
+                        <div class="form-grid">
+                            <div class="form-grid-left">
+                                <p>
+                                    <label>Fullname<span>*</span></label>
+                                    <input type="text" placeholder="Fullname" required>
+                                </p>
+                                <p>
+                                    <label>Username<span>*</span></label>
+                                    <input type="text" placeholder="Username" required>
+                                </p>
+                                <p>
+                                    <label>Password<span>*</span></label>
+                                    <input type="password" placeholder="Password" required>
+                                </p>
+                                <p>
+                                    <label>Re-type Password<span>*</span></label>
+                                    <input type="password" placeholder="Password" required>
+                                </p>
+                            </div>
+                            <div class="form-grid-right">
+                                <p>
+                                    <label>Email address<span>*</span></label>
+                                    <input type="text" placeholder="Email" required>
+                                </p>
+                                <p>
+                                    <label>Phone<span>*</span></label>
+                                    <input type="text" placeholder="Phone" required>
+                                </p>
+                            </div>
+                        </div>
+                       
+                        
+                            <input type="submit" value="Log In">
+                        </p>
+                        <p>
+                            <a href="">Forgot password?</a>
+                        </p>
+
+                    </form>
+                </div>
+                
+                
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
